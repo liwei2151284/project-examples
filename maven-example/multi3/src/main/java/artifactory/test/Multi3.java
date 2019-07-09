36e173aae9cc98fa1159cabf503a019ca2829b29
liwei2151284 <445169331@qq.com>
2019-07-09 18:04:58 +0800

    Update Multi3.java

--- maven-example/multi3/src/main/java/artifactory/test/Multi3.java
+++ maven-example/multi3/src/main/java/artifactory/test/Multi3.java
@@ -7,6 +7,9 @@ public class Multi3 {
     public static void main(String[] args) {
         new Multi1();
         System.out.println("Hello World!");
+                System.out.println("Hello World!");
+                System.out.println("Hello World!");
+                System.out.println("Hello World!");
         
     }
 }
