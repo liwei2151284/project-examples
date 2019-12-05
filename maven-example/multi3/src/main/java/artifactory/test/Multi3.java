c8dcdeb12940546c07bc817e3bbd64f01c16d2c2
liwei2151284 <445169331@qq.com>
2019-12-05 17:45:51 +0800

    Update Multi3.java
    #PROJ-100 fixed issue

--- maven-example/multi3/src/main/java/artifactory/test/Multi3.java
+++ maven-example/multi3/src/main/java/artifactory/test/Multi3.java
@@ -9,7 +9,7 @@ public class Multi3 {
         System.out.println("Hello World!");
                 System.out.println("Hello World!");
                 System.out.println("Hello World!");
-                System.out.println("Hello World!");
+
         
     }
 }
